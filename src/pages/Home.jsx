fb8a8465081f1844671f6523c139814832da8a4a
Waqarabbas72 <waqarabas72@gmail.com>
2024-05-29 21:19:57 +0500

    change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Navigation from "../components/Navigation";
 import Hero from "../components/Hero";
 import Cards from "../components/Cards";
+import Slider from "../components/Slider";
 
 const Home = () => {
   return (
     <>
       <Navigation />
       <Hero />
+      <Slider />
       <Cards />
     </>
   );
